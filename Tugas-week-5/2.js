714920f8f36f1f29b30835062486411cbd295d93
akbarakma <akbarajo1234@gmail.com>
2020-01-25 15:03:28 +0700

    tambah 2

--- Tugas-week-5/2.js
+++ Tugas-week-5/2.js
@@ -10,10 +10,76 @@
  *  Implementasikan function beliTermurah yang akan menerima input berupa array berisi minuman yang ingin dibeli oleh user.
  *  Function akan mengoutput hasil berupa harga dan rekomendasi tempat membeli minuman-minuman tersebut dengan total harga 
  *  yang paling murah.
- * @param {Array} toBuy
-   @param {Object} sellers
  */
 function beliTermurah(toBuy) {
+	var sellers = {
+		BobaFett: {
+			items: [
+				{ name: 'Fett Drink', price: 30000 },
+				{ name: 'Dragon Boba Juice', price: 40000 },
+				{ name: 'Star Big Boba', price: 25000 }
+			],
+			discount: 0.15
+		},
+		BobaFest: {
+			items: [
+				{ name: 'Fett Drink', price: 30000 },
+				{ name: 'Dragon Boba Juice', price: 40000 },
+				{ name: 'Star Big Boba', price: 25000 }
+			],
+			discount: 0.35
+		},
+		ChatTime: {
+			items: [
+				{ name: 'Fett Drink', price: 18000 },
+				{ name: 'Dragon Boba Juice', price: 24000 },
+				{ name: 'Star Big Boba', price: 21000 }
+			],
+			discount: 0.1
+		},
+		Kokumi: {
+			items: [
+				{ name: 'Fett Drink', price: 32000 },
+				{ name: 'Dragon Boba Juice', price: 42000 },
+				{ name: 'Star Big Boba', price: 27000 }
+			],
+			discount: 0.4
+		}
+  };
+    if(toBuy.length === 0){
+      return 'Pembeli Tidak Membeli Apa-Apa';
+    }
+    var hasil = '';
+    for(var i = 0 ; i < toBuy.length ; i ++){
+      var harga = [];
+      for(var keys in sellers){
+        var temp;
+        for(var j = 0 ; j < sellers[keys].items.length ; j ++){
+          if(sellers[keys].items[j].name === toBuy[i]){
+            temp = sellers[keys].items[j].price;
+          }
+        }
+        var dis = temp * sellers[keys].discount;
+        temp -= dis;
+        harga.push([temp , keys]);
+      }
+      for(var j = 0 ; j < harga.length ; j ++){
+        for(var k = 0 ; k < harga.length - 1 - j ; k ++){
+          if(harga[k][0] > harga[k+1][0]){
+            var tampung = harga[k];
+            harga[k] = harga[k+1];
+            harga[k+1] = tampung;
+          }
+        }
+      }
+
+      hasil += `${toBuy[i]} bisa dibeli dengan harga paling murah ${harga[0][0]} di ${harga[0][1]}`
+      if(i !== toBuy.length-1){
+        hasil += '\n'
+      }
+    }
+
+    return hasil;
 
 }
 
